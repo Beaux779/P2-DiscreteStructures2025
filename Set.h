09faecdceb10b139c1827ade11c17cf89c0e6965
Beaux779 <beauxrdolf@icloud.com>
2025-10-02 13:49:09 -0500

    resize&contains done. Prepped Set.cpp for function definitions

--- Set.h
+++ Set.h
@@ -15,17 +15,16 @@ class Set
         void resize();
 
     public:
-        Set();
-        Set(const Set& other);
-        ~Set();
-        Set& operator=(const Set& other);
-
-        void insert(const string& elem);
-        bool contains(const string& elem) const;
-        int getSize() const;
-        string getElement(int index) const;
-        Set intersection(const Set& other) const;
-        void print(ostream& out) const;
+        Set();          //default constructor
+        Set(const Set& other);          //copy constructor
+        ~Set();     //destructor
+        Set& operator=(const Set& other);   //assignment operator
+        void insert(const string& elem);    //insert element
+        bool contains(const string& elem) const; //check if element is in set
+        int getSize() const; //get number of elements in set
+        string getElement(int index) const; //get element at index
+        Set intersection(const Set& other) const; //intersection of two sets
+        void print(ostream& out) const; //print elements of set
 };
 
 #endif
